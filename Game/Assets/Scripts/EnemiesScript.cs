2eb3c734d7bff82fc144d4f17acb3f095882738c
Kevin Masis Leandro <kevinlml@outlook.com>
2020-12-13 09:20:43 -0600

    Enemy class y cambios menores

--- Game/Assets/Scripts/EnemiesScript.cs
+++ Game/Assets/Scripts/EnemiesScript.cs
@@ -6,23 +6,41 @@ using System;
 
 public class EnemiesScript : MonoBehaviour
 {
+    // Lista de los enemigos
+    System.Collections.Generic.List<Enemy> ListEnemies;
 
-    List<nodeEnemy> ListEnemies;
+
+    // Vector3 para posicionar los bloques en la zona de trabajo
+    private Vector3 screenPosition;
+    public GameObject PlayerEnemy;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        ListEnemies = new List<nodeEnemy>();
+        
+        ListEnemies = new System.Collections.Generic.List<Enemy>();
 
         for(int i = 0; i < 8; i++)
         {
-            // Se creal el nuevo Genoma y se añade los valores de los genes
-            nodeEnemy aux = new nodeGenoma();
-            
+            // Se crea el nuevo Genoma y se añade los valores de los genes
+            Enemy aux = new Enemy();
+         
 
             // Se añade el genoma la lista de genomas
             ListEnemies.Add(aux);
+
+
+
+
+
+
+
+
+
+            screenPosition = new Vector3(1f, 1f, 1f);
+            GameObject a = Instantiate(PlayerEnemy) as GameObject;
+            a.transform.position = screenPosition;
         }
 
 
@@ -32,173 +50,16 @@ public class EnemiesScript : MonoBehaviour
     void Update()
     {
 
-    }
-
-
-
-
-
-}
-
-
-public class nodeEnemy : MonoBehaviour
-{
-
-    // GlobalStateManager
-    public GlobalStateManager globalManager;
-    
-    // Genoma del enemigo 
-    private nodeGenoma enemyGenoma;
-
-
-
-    //___________________________________________
-   
-    //Player parameters
-    [Range(1, 2)] //Enables a nifty slider in the editor
-    public int playerNumber;
-    public float moveSpeed = 5f;
-    private int bombs = 5;
-
-
-    public bool canDropBombs = true;
-    //Can the player drop bombs?
-    public bool canMove = true;
-    //Can the player move?
-    public bool dead = false;
-
-
-
-    //Prefabs
-    public GameObject bombPrefab;
-
-    //Cached components
-    private Rigidbody rigidBody;
-    private Transform myTransform;
-    private Animator animator;
-
-    // Método Constructor
-
-    public nodeEnemy()
-    {
-
-    }
-
-    public nodeEnemy(nodeGenoma aux)
-    {
-        enemyGenoma = aux;
-        AsignarGenoma;
-    }
-
-    // Use this for initialization________________________________________________________________________________________
-    void Start()
-    {
-        //Cache the attached components for better performance and less typing
-        rigidBody = GetComponent<Rigidbody>();
-        myTransform = transform;
-        animator = myTransform.Find("PlayerModel").GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        animator.SetBool("Walking", false);
-
-        if (!canMove)
-        { //Return if player can't move
-            return;
-        }
-
-        UpdateEnemyMovement(6);
-    }
-
-  
-    // Updates Enemy movement and facing rotation using the WASD keys and drops bombs using Space
-    private void UpdateEnemyMovement(int movkey)
-    {
-        if (movkey == 1)
-        { //Up movement
-            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, moveSpeed);
-            myTransform.rotation = Quaternion.Euler(0, 0, 0);
-            animator.SetBool("Walking", true);
-        }
-
-        if (movkey == 2)
-        { //Left movement
-            rigidBody.velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
-            myTransform.rotation = Quaternion.Euler(0, 270, 0);
-            animator.SetBool("Walking", true);
-        }
-
-        if (movkey == 3)
-        { //Down movement
-            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -moveSpeed);
-            myTransform.rotation = Quaternion.Euler(0, 180, 0);
-            animator.SetBool("Walking", true);
-        }
-
-        if (movkey == 4)
-        { //Right movement
-            rigidBody.velocity = new Vector3(moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
-            myTransform.rotation = Quaternion.Euler(0, 90, 0);
-            animator.SetBool("Walking", true);
-        }
-
-        if (canDropBombs && movkey == 5)
-        { //Drop bomb
-            DropBomb();
-        }
-    }
-
-
-
-    /// <summary>
-    /// Drops a bomb beneath the player
-    /// </summary>
-    private void DropBomb()
-    {
-        if (bombPrefab)
-        { //Check if bomb prefab is assigned first
-            Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x),
-                bombPrefab.transform.position.y, Mathf.RoundToInt(myTransform.position.z)),
-                bombPrefab.transform.rotation);
-
-        }
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Explosion"))
+        for(int i = 0; i < 8; i++)
         {
-            Debug.Log("P" + playerNumber + " hit by explosion!");
-            dead = true; // 1
-            globalManager.PlayerDied(playerNumber); // 2
-            Destroy(gameObject); // 3  
-
+            ListEnemies[i].UpdateEnemyMovement(6);
         }
+       
     }
 
-    // Asignar valores del genoma al enemigo 
-
-    private void AsignarGenoma()
-    {
-        bombs = enemyGenoma.gen_bombas_numero;
-        //enemyGenoma.gen_bomba_cruz;
-        //enemyGenoma.gen_bomba_potencia;
-        //enemyGenoma.gen_curarse;
-        //enemyGenoma.gen_enfermedad;
-        //enemyGenoma.gen_esconderse;
-        //enemyGenoma.gen_lanzamiento;
-        //enemyGenoma.gen_protection;
-        //enemyGenoma.gen_suerte;
-        moveSpeed = (float)enemyGenoma.gen_velocidad;
-        //enemyGenoma.gen_vidas;
 
 
-        playerNumber = enemyGenoma.ID;
-
-
-    }
 
 
 }
+
---
+++ Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Enemy : MonoBehaviour
+{
+
+    // GlobalStateManager
+    public GlobalStateManager globalManager;
+
+    // Genoma del enemigo 
+    private nodeGenoma enemyGenoma;
+
+
+
+
+    //___________________________________________
+
+    //Player parameters
+    [Range(1, 2)] //Enables a nifty slider in the editor
+    public int playerNumber;
+    public float moveSpeed;
+    private int bombs;
+
+
+    public bool canDropBombs = true;
+    //Can the player drop bombs?
+    public bool canMove = true;
+    //Can the player move?
+    public bool dead = false;
+
+
+
+    //Prefabs
+    public GameObject bombPrefab;
+    public GameObject enemymodel;
+
+    //Cached components
+    private Rigidbody rigidBody;
+    private Transform myTransform;
+    private Animator animator;
+
+    // Método Constructor
+
+    public Enemy()
+    {
+
+    }
+
+    public Enemy(nodeGenoma aux, Material _EnemySkin)
+    {
+        enemyGenoma = aux;
+        this.gameObject.GetComponent<MeshRenderer>().material = _EnemySkin;
+
+        
+
+        AsignarGenoma();
+    }
+
+    // Use this for initialization________________________________________________________________________________________
+    void Start()
+    {
+        //Cache the attached components for better performance and less typing
+        rigidBody = GetComponent<Rigidbody>();
+        myTransform = transform;
+        animator = myTransform.Find("PlayerModel").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Walking", false);
+
+        if (!canMove)
+        { //Return if player can't move
+            return;
+        }
+
+        UpdateEnemyMovement(6);
+    }
+
+
+    // Updates Enemy movement and facing rotation using the WASD keys and drops bombs using Space
+    public void UpdateEnemyMovement(int mov)
+    {
+        if (mov == 1)
+        { //Up movement
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 0, 0);
+            animator.SetBool("Walking", true);
+        }
+
+        if (mov == 2)
+        { //Left movement
+            rigidBody.velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 270, 0);
+            animator.SetBool("Walking", true);
+        }
+
+        if (mov == 3)
+        { //Down movement
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 180, 0);
+            animator.SetBool("Walking", true);
+        }
+
+        if (mov == 4)
+        { //Right movement
+            rigidBody.velocity = new Vector3(moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 90, 0);
+            animator.SetBool("Walking", true);
+        }
+
+        if (canDropBombs && mov == 5)
+        { //Drop bomb
+            DropBomb();
+        }
+    }
+
+    // Drops a bomb
+    private void DropBomb()
+    {
+        if (bombPrefab)
+        { //Check if bomb prefab is assigned first
+            Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x),
+                bombPrefab.transform.position.y, Mathf.RoundToInt(myTransform.position.z)),
+                bombPrefab.transform.rotation);
+
+        }
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Explosion"))
+        {
+            Debug.Log("P" + playerNumber + " hit by explosion!");
+            dead = true; // 1
+            globalManager.PlayerDied(playerNumber); // 2
+            Destroy(gameObject); // 3  
+
+        }
+    }
+
+    // Asignar valores del genoma al enemigo 
+    private void AsignarGenoma()
+    {
+        bombs = enemyGenoma.gen_bombas_numero;
+        //enemyGenoma.gen_bomba_cruz;
+        //enemyGenoma.gen_bomba_potencia;
+        //enemyGenoma.gen_curarse;
+        //enemyGenoma.gen_enfermedad;
+        //enemyGenoma.gen_esconderse;
+        //enemyGenoma.gen_lanzamiento;
+        //enemyGenoma.gen_protection;
+        //enemyGenoma.gen_suerte;
+        moveSpeed = (float)enemyGenoma.gen_velocidad;
+        //enemyGenoma.gen_vidas;
+
+
+        playerNumber = enemyGenoma.ID;
+
+
+    }
+
+
+}
