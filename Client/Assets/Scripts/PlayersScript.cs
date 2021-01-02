668f1f63f3b15802b8ba98d480bb5569c8ce80d7
Adrian Gomez <adriangomez2099@gmail.com>
2021-01-02 07:40:45 -0600

    Anexos

--- Client/Assets/Scripts/PlayersScript.cs
+++ Client/Assets/Scripts/PlayersScript.cs
@@ -63,7 +63,7 @@ public class PlayersScript : MonoBehaviour
     /// Start is called before the first frame update
     void Start()
     {
-
+        Application.targetFrameRate = 60;
         Debug.Log("Entro");
 
         try
