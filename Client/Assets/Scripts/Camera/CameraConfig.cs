3b42378b86cd46653330269a7373e63ab3ce0416
Andres E. Rojas M <anderojasm@hotmail.com>
2021-01-01 21:56:06 -0600

    Camera Fix

--- Client/Assets/Scripts/Camera/CameraConfig.cs
+++ Client/Assets/Scripts/Camera/CameraConfig.cs
@@ -24,25 +24,27 @@ public class CameraConfig : MonoBehaviour
      */
     public void Start()
     {
-         if(seeMap.NColumnas_Map==50 && seeMap.NFilas_Map == 50)
+        if (seeMap.NColumnas_Map == 50 && seeMap.NFilas_Map == 50)
         {
-            Maincamera.transform.position = new Vector3(22.5f, 52.5f, 2.5f);
-            PathfingArea.transform.position = new Vector3(24.5f, 2f, 24.5f);
+            Maincamera.transform.position = new Vector3(24.5f, 59f, 24.5f);
+            //PathfingArea.transform.position = new Vector3(24.5f, 2f, 24.5f);
         }
-        else if(seeMap.NColumnas_Map == 25 && seeMap.NFilas_Map == 25)
+        else if (seeMap.NColumnas_Map == 25 && seeMap.NFilas_Map == 25)
         {
-            Maincamera.transform.position = new Vector3(11.5f, 28f, 0.5f);
-            PathfingArea.transform.position = new Vector3(12f, 2f, 12f);
+            Maincamera.transform.position = new Vector3(12f, 31f, 12f);
+            //PathfingArea.transform.position = new Vector3(12f, 2f, 12f);
         }
         else if (seeMap.NColumnas_Map == 20 && seeMap.NFilas_Map == 20)
         {
-            Maincamera.transform.position = new Vector3(9f, 24f, 9f);
-            PathfingArea.transform.position = new Vector3(9.5f, 2f, 9.5f);
+            Maincamera.transform.position = new Vector3(9.5f, 25f, 9.5f);
+            //PathfingArea.transform.position = new Vector3(9.5f, 2f, 9.5f);
         }
-        else if(seeMap.NColumnas_Map == 15 && seeMap.NFilas_Map == 15)
+        else if (seeMap.NColumnas_Map == 15 && seeMap.NFilas_Map == 15)
         {
-            Maincamera.transform.position = new Vector3(6.5f, 18f, 0.2f);
-            PathfingArea.transform.position = new Vector3(7f, 2f, 7f);
-        }    
+            Maincamera.transform.position = new Vector3(7f, 20f, 7f);
+            // PathfingArea.transform.position = new Vector3(7f, 2f, 7f);
+        }
+
+        Maincamera.transform.rotation = Quaternion.Euler(90f, 0f, 90f);
     }
 }
