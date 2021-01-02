8191c74198abd90a711ce56d41e3043af3dd5dd8
Kevin Masis Leandro <kevinlml@outlook.com>
2021-01-01 19:37:44 -0600

    Fix playersScript

--- Client/Assets/Scripts/PlayersScript.cs
+++ Client/Assets/Scripts/PlayersScript.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class PlayersScript : MonoBehaviour
 {
-
-    /// MenuScriptOptions
+   
+    /// Menu Manager Script
     public MenuManagerScript mms;
 
+    /// Global State Manager
+    public GlobalStateManager globalManager;
 
     /// Lista de los enemigos
-    private List<Players> ListPlayers;
+    private List<GameObject> ListPlayers;
 
     /// Vector3 para posicionar los bloques en la zona de trabajo
     private Vector3 screenPosition;
@@ -24,8 +26,11 @@ public class PlayersScript : MonoBehaviour
     /// Numero de Columnas del mapa
     public static int NCMap = 25;
 
-    /// Cantidad de enemgos
+    /// Cantidad de Jugadores
     public static int nPlayers = 6;
+
+    /// Cantidad de Enemigos
+    public static int nEnemies = 0;
 
     // Matriz del Menu
     public float[,] mapMatriz;
@@ -39,10 +44,12 @@ public class PlayersScript : MonoBehaviour
         try
         {
             mms = FindObjectOfType<MenuManagerScript>();
-            nPlayers = mms.nPlayers - 2;
+            nPlayers = mms.nPlayers;
+            nEnemies = mms.nEnemies;
             MFMap = mms.heightMap;
             NCMap = mms.widthMap;
             mapMatriz = mms.mapMatriz;
+
         }
         catch (System.Exception e)
         {
@@ -51,7 +58,7 @@ public class PlayersScript : MonoBehaviour
 
 
 
-        ListPlayers = new List<Players>();
+        ListPlayers = new List<GameObject>();
 
         for (int i = 0; i < nPlayers; i++)
         {
@@ -78,28 +85,48 @@ public class PlayersScript : MonoBehaviour
                     break;
             }
 
-
-            // Se crea el nuevo Genoma y se añade los valores de los genes
-            Enemy aux = new Enemy();
-
-
-            // Se añade el genoma la lista de genomas
-            Players.Add(aux);
-
-            GameObject a = Instantiate(PlayerEnemy) as GameObject;
+            GameObject a = Instantiate(PlayerPrefab) as GameObject;
             a.transform.position = screenPosition;
+
+            a.GetComponent<Players>().playerNumber = i;
+
+            ListPlayers.Add(a);
+
         }
 
 
-    }
-
-    /// Update is called once per frame
-    void Update()
-    {
-
-        for (int i = 0; i < NEnemies; i++)
+        for (int i = 0; i < nPlayers; i++)
         {
-            ListEnemies[i].UpdateEnemyMovement(6);
+
+            switch (i)
+            {
+                case 0:
+                    screenPosition = new Vector3(1f, 2f, NCMap - 2f);
+                    break;
+                case 1:
+                    screenPosition = new Vector3(MFMap - 2f, 2f, 1f);
+                    break;
+                case 2:
+                    screenPosition = new Vector3(MFMap / 2, 2f, 1f);
+                    break;
+                case 3:
+                    screenPosition = new Vector3(MFMap / 2, 2f, NCMap - 2f);
+                    break;
+                case 4:
+                    screenPosition = new Vector3(MFMap - 2f, 2f, NCMap / 2);
+                    break;
+                case 5:
+                    screenPosition = new Vector3(1F, 2f, NCMap / 2);
+                    break;
+            }
+
+            GameObject a = Instantiate(PlayerPrefab) as GameObject;
+            a.transform.position = screenPosition;
+
+            a.GetComponent<Players>().playerNumber = i;
+
+            ListPlayers.Add(a);
+
         }
 
     }
